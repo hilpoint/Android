adc975008b43e3b88a7608eabb628029e7320357
hilpoint <hilpoint@gmail.com>
2015-12-05 16:27:15 +0900

    EditText get text

--- T02_UiWidget/app/src/main/java/com/example/c/t02_uiwidget/MainActivity.java
+++ T02_UiWidget/app/src/main/java/com/example/c/t02_uiwidget/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.c.t02_uiwidget;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         myCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Toast.makeText(getApplicationContext(),"checked",
+                Toast.makeText(getApplicationContext(), "checked",
                         Toast.LENGTH_LONG).show();
             }
         });
@@ -42,12 +45,31 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 switch (checkedId) {
                     case R.id.myR1:
+                        Toast.makeText(getApplicationContext(),"myR1",
+                                Toast.LENGTH_LONG).show();
                         break;
                     case R.id.myR2:
+                        Toast.makeText(getApplicationContext(),"myR2",
+                                Toast.LENGTH_LONG).show();
                         break;
                     case R.id.myR3:
+                        Toast.makeText(getApplicationContext(),"myR3",
+                                Toast.LENGTH_LONG).show();
                         break;
                 }
+            }
+        });
+
+        final EditText myEdit = (EditText) findViewById(R.id.myEdit);
+
+        Button myButton = (Button) findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), myEdit.getText(),
+                        Toast.LENGTH_LONG).show();
+
+                myEdit.setText("");
             }
         });
     }
