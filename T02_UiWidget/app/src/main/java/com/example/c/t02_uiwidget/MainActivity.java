488e6be77d55a32bf93095facc4f51d7072491fe
hilpoint <hilpoint@gmail.com>
2015-12-05 16:05:23 +0900

    checkbox listener

--- T02_UiWidget/app/src/main/java/com/example/c/t02_uiwidget/MainActivity.java
+++ T02_UiWidget/app/src/main/java/com/example/c/t02_uiwidget/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.c.t02_uiwidget;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,15 +15,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final CheckBox myCheckBox = (CheckBox)findViewById(R.id.myCheckBox);
-        myCheckBox.setOnClickListener(new View.OnClickListener() {
+//        myCheckBox.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(myCheckBox.isChecked()) {
+//                    Toast.makeText(getApplicationContext(), "checkbox checked",
+//                            Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "checkbox unchecked",
+//                            Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+
+        myCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                if(myCheckBox.isChecked()) {
-                    Toast.makeText(getApplicationContext(), "checkbox checked",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "checkbox unchecked",
-                            Toast.LENGTH_LONG).show();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(getApplicationContext(),"checked",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RadioGroup myRGroup = (RadioGroup) findViewById(R.id.myRGroup);
+        myRGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.myR1:
+                        break;
+                    case R.id.myR2:
+                        break;
+                    case R.id.myR3:
+                        break;
                 }
             }
         });
