14342d8569324d54f7fbef2616f2cbd79ad58f61
hilpoint <hilpoint@gmail.com>
2015-12-06 17:44:07 +0900

    익스플로러 생성

--- P03_explorer/app/src/main/java/com/example/c/p03_explorer/MainActivity.java
+++ P03_explorer/app/src/main/java/com/example/c/p03_explorer/MainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<MyData> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +21,43 @@ public class MainActivity extends AppCompatActivity {
 
         String path = Environment.getExternalStorageDirectory().toString();
         File f = new File(path);
+        int icon = 0;
 
         if (f.isDirectory()) {
             File[] files = f.listFiles();
 
             for(int i = 0; i < files.length; i++) {
                 File child = files[i];
+                String name = child.getName();
 
-                Log.d("EX", child.getName());
+                if (child.isDirectory()) {
+                    icon = R.drawable.ic_folder_black_48dp;
+                } else {
+                    if (isPicture(name)) {
+                        icon = R.drawable.ic_photo_black_48dp;
+                    } else {
+                        icon = R.drawable.ic_insert_drive_file_black_48dp;
+                    }
+                }
+                Log.d(i + " file_name", name);
+
+                Log.d(i + " file_size", String.valueOf(child.length()));
+
+                list.add(new MyData(name, String.valueOf(child.length()), icon));
             }
+
         }
+
+        MyAdapter myAdapter = new MyAdapter(list, MainActivity.this);
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        myListView.setAdapter(myAdapter);
+
+    }
+
+    public boolean isPicture(String fileName) {
+        Pattern pattern = Pattern.compile("\\.(jpg|jpeg|png|gif)$", Pattern.CASE_INSENSITIVE);
+
+        Matcher matcher = pattern.matcher(fileName);
+        return matcher.find();
     }
 }
---
+++ P03_explorer/app/src/main/java/com/example/c/p03_explorer/MyData.java
@@ -0,0 +1,16 @@
+package com.example.c.p03_explorer;
+
+/**
+ * Created by c on 2015-12-06.
+ */
+public class MyData {
+    String file_name;
+    String file_size;
+    int imgIcon;
+
+    public MyData(String file_name, String file_size, int imgIcon) {
+        this.file_name = file_name;
+        this.file_size = file_size;
+        this.imgIcon = imgIcon;
+    }
+}
