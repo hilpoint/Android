884a0853e358aa76046b08caa9ac4d64b40d188c
hilpoint <hilpoint@gmail.com>
2015-12-12 17:33:50 +0900

    현재재생시간 로그찍는것까지 테스트

--- T15_MediaPlayer/app/src/main/java/com/mycompany/t15_mediaplayer/MainActivity.java
+++ T15_MediaPlayer/app/src/main/java/com/mycompany/t15_mediaplayer/MainActivity.java
@@ -4,18 +4,40 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
     MediaPlayer mp = null;
+
+    class MyThread extends Thread {
+        @Override
+        public  void run() {
+            while (true) {
+                if (mp != null && mp.isPlaying()) {
+                    try {
+                        sleep(1000);
+                        Log.d("현재재생시간", String.valueOf(mp.getCurrentPosition()));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyThread th = new MyThread();
+        th.start();
 
         Button btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
