9bbe0e4d0cf667dbfd2a728fdd1155d5386a4fc8
hilpoint <hilpoint@gmail.com>
2015-12-12 14:34:30 +0900

    Notification 클릭하면 사라짐

--- T12_notification/app/src/main/java/com/mycompany/t12_notification/Main2Activity.java
+++ T12_notification/app/src/main/java/com/mycompany/t12_notification/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.mycompany.t12_notification;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(1234);
     }
 }
