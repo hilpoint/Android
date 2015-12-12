6dfb0f395135e47401b4ecd8cbd790586c5185b5
hilpoint <hilpoint@gmail.com>
2015-12-12 15:45:11 +0900

    쓰레드로 UI 컴퍼넌트 수정 테스트

--- T13_Thread/app/src/main/java/com/mycompany/t13_thread/MainActivity.java
+++ T13_Thread/app/src/main/java/com/mycompany/t13_thread/MainActivity.java
@@ -1,21 +1,37 @@
 package com.mycompany.t13_thread;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int TEXTVIEW_VALUE = 1;
+    TextView textView;
+
+    Handler handler = new Handler() {
+      public void handleMessage(Message msg) {
+          if (msg.what == TEXTVIEW_VALUE) {
+              textView.setText("value : " + msg.arg1);
+          }
+      }
+    };
 
     class  MyThread extends Thread {
         public void run(){
             for(int i = 0; i < 100; i++) {
 
                 try {
-                    sleep(300);
+                    sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Log.d("TEST", "value : " + i);
+
+                Message msg = handler.obtainMessage();
+                msg.what = TEXTVIEW_VALUE;
+                msg.arg1 = i;
+                handler.sendMessage(msg);
             }
         }
     }
@@ -24,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("setText test");
 
         MyThread th = new MyThread();
         th.start();
