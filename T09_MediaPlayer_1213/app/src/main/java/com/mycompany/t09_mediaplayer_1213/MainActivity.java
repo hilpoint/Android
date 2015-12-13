08e4632af1846487d2a58ef6467d39a33a48be6e
hilpoint <hilpoint@gmail.com>
2015-12-13 13:46:08 +0900

    미디어 물레이버 복습

---
+++ T09_MediaPlayer_1213/app/src/main/java/com/mycompany/t09_mediaplayer_1213/MainActivity.java
@@ -0,0 +1,48 @@
+package com.mycompany.t09_mediaplayer_1213;
+
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = Environment.getExternalStorageDirectory() + "/Music/어깨가 들썩들썩 신나는 노래 모음/2NE1 - Fire.mp3";
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(path);
+                    mediaPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mediaPlayer.start();
+            }
+        });
+
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                }
+                mediaPlayer = null;
+            }
+        });
+    }
+}
