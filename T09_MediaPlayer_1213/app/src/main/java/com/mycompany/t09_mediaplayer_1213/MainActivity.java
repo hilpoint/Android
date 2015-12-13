2637486c067f2ba1e95b8b8d69cb57e73e0abc3c
hilpoint <hilpoint@gmail.com>
2015-12-13 14:58:53 +0900

    시크바와 프로그래스바 연동

--- T09_MediaPlayer_1213/app/src/main/java/com/mycompany/t09_mediaplayer_1213/MainActivity.java
+++ T09_MediaPlayer_1213/app/src/main/java/com/mycompany/t09_mediaplayer_1213/MainActivity.java
@@ -2,22 +2,74 @@ package com.mycompany.t09_mediaplayer_1213;
 
 import android.media.MediaPlayer;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer = null;
+    SeekBar seekBar;
+    TextView textPlayTime =null;
+    TextView textDuration =null;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 1) {
+                seekBar.setProgress(msg.arg1);
+
+                int totalSeconds = msg.arg1 / 1000;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds % 60;
+                textPlayTime.setText(String.format("%02d", minutes) + " : " + String.format("%02d", seconds));
+            }
+        }
+
+    };
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    if (mediaPlayer != null) {
+                        if (mediaPlayer.isPlaying()) {
+                            sleep(100);
+
+                            //seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                            Message msg = handler.obtainMessage();
+                            msg.what = 1;
+                            msg.arg1 = mediaPlayer.getCurrentPosition();
+                            handler.sendMessage(msg);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textPlayTime = (TextView) findViewById(R.id.textPlayTime);
+        textDuration  = (TextView) findViewById(R.id.textDuration);
         Button btnStart = (Button) findViewById(R.id.btnStart);
+
+        MyThread th = new MyThread();
+        th.start();
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +82,13 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+
+                int totalSeconds = mediaPlayer.getDuration() / 100;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds % 60;
+
+                textDuration.setText(minutes + ":" + seconds);
             }
         });
 
@@ -42,6 +101,26 @@ public class MainActivity extends AppCompatActivity {
                     mediaPlayer.release();
                 }
                 mediaPlayer = null;
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    if (mediaPlayer != null)
+                        mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
