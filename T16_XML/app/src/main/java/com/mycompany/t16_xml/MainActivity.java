ec4d71658f3e3e199ae642557390c2d39cd590f5
hilpoint <hilpoint@gmail.com>
2015-12-19 17:00:40 +0900

    xml 파싱 테스트

---
+++ T16_XML/app/src/main/java/com/mycompany/t16_xml/MainActivity.java
@@ -0,0 +1,83 @@
+package com.mycompany.t16_xml;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    class MyXMLTask extends AsyncTask<String, Void, Document>{
+
+        private String getElementText(Element dataElement, String tag) {
+            NodeList tagNodeList = dataElement.getElementsByTagName(tag);
+            Element tagElement =  (Element) tagNodeList.item(0);
+
+            NodeList tagTextNodeList = tagElement.getChildNodes();
+            return tag + " : " + tagTextNodeList.item(0).getNodeValue();
+        }
+
+        @Override
+        protected void onPostExecute(Document document) {
+            super.onPostExecute(document);
+
+            String str = "";
+            NodeList  nodeList
+                    = document.getElementsByTagName("data");
+
+            for (int i=0; i<nodeList.getLength(); i++){
+                Element dataElement = (Element) nodeList.item(i);
+
+                str += getElementText(dataElement, "day");
+                str += getElementText(dataElement, "hour");
+                str += getElementText(dataElement, "temp");
+                str += getElementText(dataElement, "wfKor");
+            }
+            textView.setText(str);
+        }
+
+
+        @Override
+        protected Document doInBackground(String... params) {
+            URL url =  null;
+            Document doc = null;
+
+            try {
+                url = new URL(params[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                doc = db.parse(url.openStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return doc;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        Button btnStart = (Button) findViewById(R.id.btnsStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MyXMLTask().execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153054000");
+            }
+        });
+    }
+}
