64e895440458fe4d8f6642819076016664871488
hilpoint <hilpoint@gmail.com>
2015-12-26 15:39:47 +0900

    style 코드 완성

--- T22_style/app/src/main/java/com/mycompany/t22_style/MainActivity.java
+++ T22_style/app/src/main/java/com/mycompany/t22_style/MainActivity.java
@@ -1,13 +1,41 @@
 package com.mycompany.t22_style;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView selectedTextView, workingTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectedTextView = (TextView) findViewById(R.id.selectedTextView);
+        workingTextView = (TextView) findViewById(R.id.workingTextView);
+
+
+        Button btnApply = (Button) findViewById(R.id.btnApply);
+        btnApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+
+        View.OnClickListener numberButtonListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        Button btnZero = (Button) findViewById(R.id.btnZero);
+        Button btnOne = (Button) findViewById(R.id.btnOne);
+
+        btnZero.setOnClickListener(numberButtonListener);
+        btnOne.setOnClickListener(numberButtonListener);
     }
 }
