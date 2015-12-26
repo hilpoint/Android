1b40cfd5af696d42aabea9ff1e44bf5b7671aaab
hilpoint <hilpoint@gmail.com>
2015-12-26 12:20:24 +0900

    fragment 예제 실습

---
+++ T20_fragment/app/src/main/java/com/mycompany/t20_fragment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mycompany.t20_fragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
