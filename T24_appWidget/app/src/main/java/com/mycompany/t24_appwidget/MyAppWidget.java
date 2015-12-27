10fba4052fbeddcf336ee3149bd6c9a178f8f330
hilpoint <hilpoint@gmail.com>
2015-12-27 13:37:49 +0900

    appwidget 예제

---
+++ T24_appWidget/app/src/main/java/com/mycompany/t24_appwidget/MyAppWidget.java
@@ -0,0 +1,9 @@
+package com.mycompany.t24_appwidget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by c on 2015-12-27.
+ */
+public class MyAppWidget extends AppWidgetProvider {
+}
